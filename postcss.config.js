73ef83d27ef91af078da21c3129e05aaa360eafd
Miguelangel <silvermiguel96@gmail.com>
2019-03-03 23:27:34 -0500

    variable

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   plugins: [
-    require('autoprefixer')({
-      grid:true
+    // require('autoprefixer')({
+    //   grid:true
+    // }),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        grid: true,
+        flexbox: false,
+      },preserve: true, //Valor por defecto de "true".Se asemeja a customProperties
+      //cambiar a false para notar el cambio
     })
   ]
 }
